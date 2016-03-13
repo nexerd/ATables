6ca93de959f32db857a8fd82a519422254de2b9a
nexerd <vitaliy-suhov@bk.ru>
2016-03-13 16:39:47 +0400

    Создание веб-приложения.

---
+++ app/app.js
@@ -0,0 +1,44 @@
+var http = require("http");
+var express = require("express");
+var logger = require("morgan");
+var favicon = require("serve-favicon");
+var static = require("serve-static");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var fs = require("fs");
+
+var LogStream = fs.createWriteStream(__dirname + "/logger.log", {flags: 'w'});
+
+var app = express();
+
+app.set("port", process.env.PORT || 8000)
+app.set("views", __dirname + "/views");
+app.set("view exgine", "jade");
+
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(logger("dev", {stream: LogStream}));
+app.use(static(__dirname + "/public"));
+app.use(bodyParser());
+app.use(methodOverride());
+
+app.get("/", function(req, res) {
+	res.render("index.jade", {title: "Ads Table", text: "Hellow World!"});
+	});
+
+app.use(function(req, res, next){
+		throw new Error(req.url + " not found :(");
+	});
+app.use(function(err, req, res, next){
+		console.log(err);
+		res.send(err);
+	});
+	
+
+http.createServer(app).listen(app.get("port"), function()
+	{
+		console.log("Express server listening on port " + app.get("port"));
+	});
+
+
+
+
