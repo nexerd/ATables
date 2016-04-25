c98b14236cba70d0a40880969b5ed6484d3de3d1
nexerd <vitaliy-suhov@bk.ru>
2016-04-25 19:13:33 +0400

    Добавлена версия node в package,json

--- app.js
+++ app.js
@@ -79,6 +79,12 @@ app.get("/", function(req, res) {
 	});	
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handlers
 
