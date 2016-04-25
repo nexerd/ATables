041123d5db13923dccbc9a88516a1ff3bf9e77be
nexerd <vitaliy-suhov@bk.ru>
2016-04-25 18:33:18 +0400

    Подготовка приложения для развертывание в azure

--- app.js
+++ app.js
@@ -80,23 +80,30 @@ app.get("/", function(req, res) {
 });
 
 
-app.use(function(req, res, next){
-		throw new Error(req.url + " not found :(");
-	});
-app.use(function(err, req, res, next){
-		console.log(err);
-		res.send(err);
-	});
+// error handlers
 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
-	
-
-http.createServer(app).listen(app.get("port"), function()
-	{
-		console.log("Express server listening on port " + app.get("port"));
-	});
-
-
+module.exports = app;	
 
 
